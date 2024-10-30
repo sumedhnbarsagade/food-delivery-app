72f7c4c748edfcf2230720f04e4010855a72bdeb
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-10-30 20:16:42 +0530

    complete swigey api call method

--- src/components/RestaurantCards.jsx
+++ src/components/RestaurantCards.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function RestaurantCards() {
+function RestaurantCards(props) {
+    const {resData} = props;
   return (
     <div className="container">
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gp1ityra6utvzqn6ghnv" alt="Biryani" width="250px" height="200px" />
-        <h3>Meghana Foods</h3>
-        <p>Biryani, North India, Asian</p>
-        <p>4.4 Stars</p>
-        <p>38 mins</p>
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId}  alt="Biryani" className='resImage' />
+        <h3>{resData.info.name}</h3>
+        <p>{resData.info.cuisines.join(", ")}</p>
+        <p>{resData.info.avgRating} Stars</p>
+        <p>{resData.info.sla.deliveryTime} mins</p>
     </div>
   )
 }
